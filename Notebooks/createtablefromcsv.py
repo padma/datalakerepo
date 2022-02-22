2b5ea20248bd09999aab461158ae9b72382cda5e
padma <pradeep.padma@gmail.com>
2022-02-22 19:03:09 +0530

    Update createtablefromcsv.py

--- Notebooks/createtablefromcsv.py
+++ Notebooks/createtablefromcsv.py
@@ -1,3 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Overview
+# MAGIC This notebook will show you how to create and query a table or DataFrame that you uploaded to DBFS. DBFS is a Databricks File System that allows you to store data for querying inside of Databricks. This notebook assumes that you have a file already inside of DBFS that you would like to read from.
+# MAGIC 
+# MAGIC This notebook is written in Python so the default cell type is Python. However, you can use different languages by using the %LANGUAGE syntax. Python, Scala, SQL, and R are all supported.
+
+# COMMAND ----------
+
 # File location and type
 file_location = "/FileStore/tables/Country_codes.csv"
 file_type = "csv"
